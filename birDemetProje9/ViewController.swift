ed83bf6696dd6463f42613c6706cdaf92db60e6b
omer ipek <omer_furkan_ipek@hotmail.com>
2024-05-05 17:08:35 +0300

    Initial Commit

---
+++ birDemetProje9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birDemetProje9
+//
+//  Created by Ömer Furkan İpek on 5.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
